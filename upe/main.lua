c1ee9e46f499adf8cfb17e0ed9dff8d2fa0ff72f
Thiago Almeida <37854462+Th1agoAlmeida@users.noreply.github.com>
2021-09-09 22:01:08 -0300

    Add files via upload

---
+++ upe/main.lua
@@ -0,0 +1,13 @@
+-- chamar o framwork composer
+local composer = require( "composer" )
+
+-- ESCONDER BARRA DE ESTATUS
+display.setStatusBar( display.HiddenStatusBar )
+
+-- CHAMAR CENAS
+composer.gotoScene( "scene00", "fade", 1000 ) 
+
+-------- VISUALIZAÇÃO NO IPAD
+
+
+    
